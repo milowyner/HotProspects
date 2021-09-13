f4765ea13d862d741ad023fbe074c089f039f88b
Milo Wyner <milowyner@gmail.com>
2021-09-13 15:58:11 -0700

    Example 4: objectWillChange

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -7,50 +7,26 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    enum NetworkError: Error {
-        case badURL, requestFailed, unknown
-    }
-    
-    var body: some View {
-        Text("Hello, World!")
-            .onAppear {
-                fetchData(from: "https://www.apple.com") { result in
-                    switch result {
-                    case .success(let string):
-                        print(string)
-                    case.failure(let error):
-                        switch error {
-                        case .badURL:
-                            print("Bad URL")
-                        case .requestFailed:
-                            print("Network problems")
-                        case .unknown:
-                            print("Unknown error")
-                        }
-                    }
-                }
-            }
-    }
-    
-    func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
-        guard let url = URL(string: urlString) else {
-            completion(.failure(.badURL))
-            return
+class DelayedUpdater: ObservableObject {
+    var value = 0 {
+        willSet {
+            objectWillChange.send()
         }
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            DispatchQueue.main.async {
-                if let data = data {
-                    let stringData = String(decoding: data, as: UTF8.self)
-                    completion(.success(stringData))
-                } else if error != nil {
-                    completion(.failure(.requestFailed))
-                } else {
-                    completion(.failure(.unknown))
-                }
+    }
+
+    init() {
+        for i in 1...10 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.value += 1
             }
-        }.resume()
+        }
+    }
+}
+
+struct ContentView: View {
+    @ObservedObject var updater = DelayedUpdater()
+    var body: some View {
+        Text("Value is: \(updater.value)")
     }
 }
 
